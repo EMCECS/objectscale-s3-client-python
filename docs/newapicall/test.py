4704cda8e893b4bfb79099fc6d76ae6209347e43
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2022-10-07 12:12:15 -0700

    Credentials Activated
    Signed-off-by: DrayerSivertsen <drayer.sivertsen@wsu.edu>

--- docs/newapicall/test.py
+++ docs/newapicall/test.py
@@ -1,23 +1,35 @@
 import boto3
 from boto3 import Session
-import creds.py
+import urllib3
 
+urllib3.disable_warnings()
 
+with open("creds.txt") as file:
+    lines = file.readlines()
+    Endpoint = lines[0].strip()
+    Access_Key = lines[2].strip()
+    Secret_Key1 = lines[3].strip()
 
 cred_object = {
-    "aws_access_key_id": creds.Access_Key,
-    "aws_secret_access_key": creds.Secret_Key1,
-    "endpoint_url": creds.Endpoint,
+    "endpoint_url": Endpoint,
+    "aws_access_key_id": Access_Key,
+    "aws_secret_access_key": Secret_Key1,
+    "use_ssl": False,
+    "verify": False
 }
+
+# print(cred_object)
 
 client = boto3.client("s3", **cred_object)
 
-client.create_bucket(Bucket="new")
+if client is None: 
+    print("Failed")
 
-client.list_buckets()
+response = client.list_buckets()
+print(response)
 
-
-
+# use_ssl=False,
+# verify=False
 
 # c = boto3.client("s3")
 
