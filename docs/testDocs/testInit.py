4fa8720121f07b3d5b6457d6f2e08c5e5e07cd42
Killian Griffin <killiangriffin00@gmail.com>
2022-11-13 22:28:03 -0800

    Leftover from last commit
    Signed-off-by: Killian Griffin <killiangriffin00@gmail.com>

--- docs/testDocs/testInit.py
+++ docs/testDocs/testInit.py
@@ -1,3 +1,30 @@
-import mock
+from unittest import mock
 import unittest
+import urllib3, os, sys
 
+urllib3.disable_warnings()
+
+
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = '../../../creds.txt'
+abs_file_path = os.path.join(script_dir, rel_path)
+
+with open(abs_file_path) as file:
+    lines = file.readlines()
+    Endpoint = lines[0].strip()
+    Access_Key = lines[2].strip()
+    Secret_Key1 = lines[3].strip()
+
+cred_object = {
+    "endpoint_url": Endpoint,
+    "aws_access_key_id": Access_Key,
+    "aws_secret_access_key": Secret_Key1,
+    "use_ssl": False,
+    "verify": False
+}
+
+class TestSession(unittest.TestCase):
+    def setUp(self) -> None:
+        self.
+        client = session.client('s3', **cred_object)
+        return super().setUp()
