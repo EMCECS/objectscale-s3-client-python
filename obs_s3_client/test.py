342cfc466b3d51027b3c2f09b99d133c547da858
DrayerSivertsen <drayer.sivertsen@wsu.edu>
2023-02-05 17:15:22 -0800

    get metadata
    Signed-off-by: DrayerSivertsen <drayer.sivertsen@wsu.edu>
    Signed-off-by: Killian Griffin <killiangriffin00@gmail.com>

--- obs_s3_client/test.py
+++ obs_s3_client/test.py
@@ -47,14 +47,16 @@ cred_object = {
 session = Session()
 client = session.client('s3', **cred_object)
 
-res = client.list_buckets()
-
-print(res)
-
-# client.create_bucket(Bucket='mybucket', CreateBucketConfiguration={'LocationConstraint': 'us-west-2'}, SearchMetaData='Size,CreateTime,LastModified,x-amz-meta-STR;String,x-amz-meta-INT;Integer')
-
-# res = client.searchmetadata(Bucket='mybucket')
+# res = client.list_buckets()
 # print(res)
+
+res = client.create_bucket(Bucket='mybucket', CreateBucketConfiguration={'LocationConstraint': 'us-west-2'}, SearchMetaData='Size,CreateTime,LastModified,x-amz-meta-STR;String,x-amz-meta-INT;Integer')
+
+res = client.get_bucket_acl(Bucket='mybucket')
+# print(res)
+
+res = client.get_search_metadata(Bucket='mybucket')
+print(res)
 
 
 
