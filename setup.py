2f9a3f4dd8f3eccf4c196349391f685ae5b8dabe
Killian Griffin <killiangriffin00@gmail.com>
2023-02-26 21:52:09 -0800

    Successfully utilized extension method by downloading package with TestPyPi
    Signed-off-by: Killian Griffin <killiangriffin00@gmail.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "obs-s3-client",
+    version = "0.0.11",
+    description = "Open-source S3 client library in python to support ObjectScales proprietary S3 extensions",
+    author = "Drayer Sivertsen",
+    classifiers = [ 'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3.10',
+    'Topic :: Software Development :: Libraries' ],
+    keywords = [ 'ObjectScale', 'S3', 'Dell', 'boto3', 'botocore' ],
+    dependencies = [ 'boto3' ],
+    packages = find_packages()
+)
